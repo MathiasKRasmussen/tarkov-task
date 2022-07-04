535079748eb8f343a84950d1dcf3c1e309db0ac8
MathiasAAU <mkra17@student.aau.dk>
2022-07-04 15:23:05 +0200

    More json and db update

--- src/lib/db/data/maps.ts
+++ src/lib/db/data/maps.ts
@@ -1,9 +1,10 @@
-import type { Map, Trader } from '@prisma/client';
+import type { HideoutStation, Map, Trader } from '@prisma/client';
 
 import itemsData from './json/items.json';
 import tradersData from './json/traders.json';
 import mapsData from './json/maps.json'
 import typesData from './json/types.json'
+import hideoutData from './json/hideoutStations.json'
 
 
 export function createTypesList() {
@@ -12,9 +13,14 @@ export function createTypesList() {
         item.types.forEach((type) => {
             if(!types.includes(type)){
                 types.push(type)
-                console.log(type)
             }
         })
     })
-    console.log(types)
+    console.log(hideoutData.hideoutStations[0])
+    let station: HideoutStation = {id: '', name: '', constructionTime: 0, level: 0}
+    station.name = hideoutData.hideoutStations[0].name
+    station.id = hideoutData.hideoutStations[0].id
+    station.constructionTime = hideoutData.hideoutStations[0].levels[0].constructionTime
+    station.level = hideoutData.hideoutStations[0].levels[0].level
+    
 }
---
+++ src/lib/db/data/uploadData.ts
@@ -0,0 +1,10 @@
+import { prisma } from '$lib/db/prisma';
+
+export async function addHideoutStations(): Promise<any> {
+    const result = await prisma.hideoutStation.create({
+        data: {
+            
+        }
+    })
+    return result
+}
