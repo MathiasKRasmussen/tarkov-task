aef41b4b121ac928efba3e090203499fd72f2853
MathiasAAU <mkra17@student.aau.dk>
2022-08-09 21:20:35 +0200

    new dotenv

--- src/lib/db/supabaseClient.ts
+++ src/lib/db/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv';
+dotenv.config();
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = process.env['VITE_SUPABASE_URL'] ? process.env['VITE_SUPABASE_URL'] : ''; // import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = process.env['VITE_SUPABASE_ANON_KEY'] ? process.env['VITE_SUPABASE_ANON_KEY'] : ''; //import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
