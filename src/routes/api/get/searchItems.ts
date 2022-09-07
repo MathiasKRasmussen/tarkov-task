9b87d3da415148a04d77771def6ed6fe62866f06
MathiasAAU <mkra17@student.aau.dk>
2022-09-07 20:34:26 +0200

    Changed searched items to 2 letters and fixed so amount shows when db search

--- src/routes/api/get/searchItems.ts
+++ src/routes/api/get/searchItems.ts
@@ -1,11 +1,13 @@
 import { searchItems } from "$lib/db/models/items"
+import { getRequiredTaskItemsSearch } from "$lib/util/formatItems"
 import type { Item } from "@prisma/client"
 
 // Post endpoint for searching for items
 export async function post({ request }: { request: any }) {
     let data = await request.json()
     let success: boolean = true
-    const items: Item[] = await searchItems(data.searchText)
+    let tempItems: Item[] = await searchItems(data.searchText, data.$userName)
+    const items: Item[] = getRequiredTaskItemsSearch(tempItems)
     return {
         body: {
             items,
